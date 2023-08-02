6a7f601ff4e2b75b79e5b68c03ae313e4391eb65
Rashmitatamang <Rashmitatamang@email.com>
2023-08-02 17:31:52 -0500

    changed names

--- class1/Program.cs
+++ class1/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             var dic = new Dictionary<string, string>();
-            dic ["Husband"] = "Ram";
-            dic["Wife"] = "Sita";
+            dic ["Husband"] = "fin";
+            dic["Wife"] = "grace";
 
             var name3 = $"{dic["bro"]} and {dic["sis"]} are friends.";
             Console.WriteLine(name3);
