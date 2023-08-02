a1a2ab5e8bbe83b47089cde12177dbe788842d21
Rashmitatamang <Rashmitatamang@email.com>
2023-08-02 18:33:04 -0500

    change

--- class1/Program.cs
+++ class1/Program.cs
@@ -8,7 +8,7 @@
             dic ["Husband"] = "fin";
             dic["Wife"] = "grace";
 
-            var name3 = $"{dic["bro"]} and {dic["sis"]} are friends.";
+            var name3 = $"{dic["cousin"]} and {dic["sis"]} are friends.";
             Console.WriteLine(name3);
             Console.WriteLine("Hello friends 11111111");
         }
