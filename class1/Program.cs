8ad338c3acfd5e9faae59da8ee584227b7ff9f9f
Rashmitatamang <Rashmitatamang@email.com>
2023-08-02 17:07:23 -0500

    initial comment

---
+++ class1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace class1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dic = new Dictionary<string, string>();
+            dic ["Husband"] = "Ram";
+            dic["Wife"] = "Sita";
+
+            var name3 = $"{dic["Husband"]} and {dic["Wife"]} are friends.";
+            Console.WriteLine(name3);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
